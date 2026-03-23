45798208439a2d29136701ae407ce47ff0d5d5ac
Swastik011005 <swastikbasuu2005@gmail.com>
2026-03-23 09:27:44 +0530

    Implement UC4 maintain ordered bogie IDs using TreeSet

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.util.HashSet;
+import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,17 +6,18 @@ public class Main {
         System.out.println("=== Train Consist Management App ===");
         System.out.println();
 
-        HashSet<String> bogieIds = new HashSet<>();
+        TreeSet<String> bogieIds = new TreeSet<>();
 
+        bogieIds.add("BG105");
         bogieIds.add("BG101");
-        bogieIds.add("BG102");
         bogieIds.add("BG103");
+        bogieIds.add("BG102");
         bogieIds.add("BG101"); // duplicate
 
-        System.out.println("Unique bogie IDs added successfully...");
-        System.out.println("Current Bogie IDs : " + bogieIds);
-        System.out.println("Total Unique Bogies : " + bogieIds.size());
+        System.out.println("Ordered bogie IDs maintained successfully...");
+        System.out.println("Current Ordered Bogie IDs : " + bogieIds);
+        System.out.println("Total Unique Ordered Bogies : " + bogieIds.size());
         System.out.println();
-        System.out.println("Duplicate bogie IDs are automatically ignored.");
+        System.out.println("Bogie IDs are sorted automatically and duplicates are ignored.");
     }
 }
